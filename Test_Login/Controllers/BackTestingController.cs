66ed97e6712be94f7480e3f201c5678e4c0a42e0
Angus Pan <softangipan@gmail.com>
2022-03-21 16:28:28 +0800

    回測系統: 有Bug待改

--- Test_Login/Controllers/BackTestingController.cs
+++ Test_Login/Controllers/BackTestingController.cs
@@ -21,6 +21,12 @@ namespace Test_Login.Controllers
         [HttpPost]
         public ActionResult BackTesting001(string stockID, string day, string totalUnit)
         {
+            //var checkDate123456 = db.stockPrice
+            //            .Where(x => x.stockID == stockID && x.stockDate == "20210101")
+            //            .Select(x => x.endPrice)
+            //            .ToList();
+            //var gjfogdpsjfgpoji = 123252345;
+
             string[] years = { "2021", "2022" };
             string[] months = { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12" };
             double s = 1.425 / 1000;    // 手續費千分之1.425 (不計網路下單折數)
@@ -33,17 +39,38 @@ namespace Test_Login.Controllers
             string nowVal_Date = db.stockPrice       // 取得資料庫中最後收盤價的日期
                 .Where(x => x.stockID == stockID)
                 .OrderByDescending(x => x.stockDate).Select(x => x.stockDate).ToList()[0];
-            List<int> endPrice_everyMon = new List<int>(); // 存每個月固定日的收盤價
-            
+            List<double> endPrice_everyMon = new List<double>(); // 存每個月固定日的收盤價
+
             foreach (string year in years)
             {
                 foreach (string month in months)
                 {
-                    string result = db.stockPrice
-                        .Where(x => x.stockID == stockID && x.stockDate == $"{year}{month}{day}")
+                    string temp = $"{year}{month}{day}";
+                    var checkDate = db.stockPrice
+                        .Where(x => x.stockID == stockID && x.stockDate == temp)
                         .Select(x => x.endPrice)
-                        .ToList()[0];
-                    endPrice_everyMon.Add(int.Parse(result));
+                        .ToList();
+                    if (checkDate.Count != 0)
+                    {
+                        endPrice_everyMon.Add(Convert.ToDouble(checkDate[0]));
+                    }
+                    else
+                    {
+                        SqlConnection cn = new SqlConnection("Data Source=localhost;Initial Catalog=Lab;Integrated Security=True");
+                        SqlCommand cmd = new SqlCommand("select top (1)  stockDate from [stockPrice] " +
+                                $"where　stockDate > '{year}{month}{day}'　order by stockDate", cn);
+                        cn.Open();
+                        string nextday = cmd.ExecuteScalar().ToString();
+                        cn.Close();
+                        checkDate = db.stockPrice
+                        .Where(x => x.stockID == stockID && x.stockDate == nextday)
+                        .Select(x => x.endPrice)
+                        .ToList();
+                        if (checkDate.Count != 0)
+                        {
+                            endPrice_everyMon.Add(Convert.ToDouble(checkDate[0]));
+                        }
+                    }
                 }
             }
 
@@ -53,6 +80,11 @@ namespace Test_Login.Controllers
             //string result = cmd.ExecuteScalar().ToString();
             //ViewBag.result = result;
             //cn.Close();
+            int asdjoajd = 0;
+            foreach(int i in endPrice_everyMon)
+            {
+                asdjoajd += i;
+            }
             return View();
         }
         public ActionResult BackTesting002()
